80eb2eabfcc63a5e563b1101bcecc6fb1eeb9dc4
Matytsin Dmitrij <matytsindmitrij@gmail.com>
2020-08-10 19:09:12 +0300

    add close button component

---
+++ src/Components/common/inCard/CloseButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './style/CloseButton.module.css';
+
+
+const CloseButton = () => {
+    return <div className={s.closeButton}>&times;</div>
+}
+
+export {
+    CloseButton
+};
