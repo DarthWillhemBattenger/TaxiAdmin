66661b22b12007a332b8ff27e2cb3ce346238b56
Matytsin Dmitrij <matytsindmitrij@gmail.com>
2020-08-11 22:57:51 +0300

    add iOS like checkbox

--- src/Components/common/CheckBox.js
+++ src/Components/common/CheckBox.js
@@ -2,6 +2,14 @@ import React from 'react';
 import s from './style/CheckBox.module.css';
 
 const CheckBox = (props) => {
+    if (props.isiOSLikeCheckbox) {
+        return (
+            <div className={props.className}>
+                <input type="checkbox" id={props.checkBoxId}/>
+                <label className={s.iOSLikeCheckBox} htmlFor={props.checkBoxId}/>
+            </div>
+        );
+    }
 	return (
         <div className={props.className}>
             <input type="checkbox" id={props.checkBoxId}/>
